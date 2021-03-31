a4d41bc064550ffdd1134efea6e49a5b2db00f44
Duy Pham Le <phamleduy04@gmail.com>
2021-03-30 22:48:42 -0500

    upload

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
